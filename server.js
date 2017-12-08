0f87b95f64866893523fc1a1611608f28327c889
amykslover <amykslover@gmail.com>
2017-12-07 23:32:32 -0800

    Configuring MLAB for Heroku deployment

--- server.js
+++ server.js
@@ -29,6 +29,22 @@ mongoose.connect("mongodb://localhost/news", {
   useMongoClient: true
 });
 
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (error, database) {
+  if (error) {
+    console.log(error);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database Connection Ready");
+
+  // Initialize the app.
+  var server = app.listen(PORT, function () {
+    var port = server.address().port;
+    console.log("RUNNING ON PORT " + PORT + "!");
+  });
+});
 
 // A GET route for scraping the echojs website
 app.get("/scrape", function(req, res) {
@@ -117,10 +133,4 @@ app.post("/articles/:id", function(req, res) {
       // If an error occurred, send it to the client
       res.json(err);
     });
-});
-
-
-
-app.listen(PORT, function() {
-  console.log("RUNNING ON PORT " + PORT + "!");
 });
